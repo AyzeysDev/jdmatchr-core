8f9e6971cc1603012e276855471d7f0c2a96d80b
Aravind Kumar <ara3194@gmail.com>
2025-05-11 16:36:50 +1000

    Initial commit

---
+++ src/main/java/com/jdmatchr/jdmatchr_core/JdmatchrCoreApplication.java
@@ -0,0 +1,16 @@
+package com.jdmatchr.jdmatchr_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class JdmatchrCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdmatchrCoreApplication.class, args);
+		System.out.println("JDMatchr Core Spring Boot Application has started!");
+	}
+
+}
---
+++ src/test/java/com/jdmatchr/jdmatchr_core/JdmatchrCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdmatchr.jdmatchr_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdmatchrCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
